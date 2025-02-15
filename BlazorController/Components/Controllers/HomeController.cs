2467657f4b0be863ed0ddc19acce5cf0a4c21902
Henry Brooks <henrybrooks100@outlook.com>
2025-02-15 16:06:40 +0000

    Add project files.

---
+++ BlazorController/Components/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace BlazorController.Components.Controllers
+{
+    [Route("api/SignUp")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("greet")]
+        public IActionResult Greet()
+        {
+            return Ok("The controller works!");
+        }
+    }
+}
---
+++ BlazorController/Program.cs
@@ -0,0 +1,40 @@
+using BlazorController.Components;
+using Microsoft.AspNetCore.Components;
+
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();////added on
+
+//HTTP client added on
+builder.Services.AddScoped(sp =>
+{
+    var navigationManager = sp.GetRequiredService<NavigationManager>();
+    return new HttpClient { BaseAddress = new Uri(navigationManager.BaseUri) };
+});
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.MapControllers();
+app.Run();
